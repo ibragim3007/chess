46f74f34247771d821d2ea5d3315ad10964473f9
Ibragim <lol.3007@mail.ru>
2023-08-14 22:45:13 -0700

    Get ready navigate button

--- src/modules/FillNameModule/components/FillInputs/FillNameInputs.tsx
+++ src/modules/FillNameModule/components/FillInputs/FillNameInputs.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Input } from '../../../../shared/Inputs/Input';
-import { Button } from '../../../../shared/Buttons/Button';
-import { Container } from '../../../../shared/layouts/Container';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux/reduxHooks';
+import { Button } from '../../../../shared/Buttons/Button';
+import { Input } from '../../../../shared/Inputs/Input';
+import { Container } from '../../../../shared/layouts/Container';
 import { formSliceAction } from '../../store/reducers/formSlice';
+import { useNavigate } from 'react-router-dom';
+import { route } from '../../../../router/consts/routesConfig';
 
 const FillNameInputs = () => {
   const { formData } = useAppSelector(state => state.formSliceReducer);
@@ -13,17 +15,26 @@ const FillNameInputs = () => {
     dispath(formSliceAction.changeForm({ ...formData, [e.target.id]: e.target.value }));
   };
 
+  const navigate = useNavigate();
+
+  const onClickStartGame = () => {
+    navigate(route.game);
+  };
+
   return (
     <Container style={{ flexDirection: 'column', gap: 25 }}>
       <Input
         onChange={onChangeInput}
         id="name"
         placeholder="Enter name"
-        borderColor="warning"
-        size="large"
+        borderColor={formData.name.length > 2 ? 'success' : 'warning'}
+        size={formData.name ? 'large' : 'normal'}
         value={formData.name}
+        autoComplete="off"
+        autoCapitalize="off"
       />
-      <Button>Play</Button>
+
+      <Button onClick={onClickStartGame}>Play</Button>
     </Container>
   );
 };
